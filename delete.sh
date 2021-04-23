5b73ea14ad4ae3bec20e3732985b9fb3d0be9dac
Markham <quinesquines@gmail.com>
2021-04-24 06:17:42 +0800

    Fix COUNT in delete

--- delete.sh
+++ delete.sh
@@ -1,7 +1,8 @@
 . shared.sh
 
 if (($#)); then
-	PARAMS="ids[count]=$#"
+	COUNT=$#
+	PARAMS="ids[count]=$COUNT"
 	for ID in "${@}"; do
 		PARAMS+="&ids[imageids][]=$ID"
 	done
@@ -10,9 +11,14 @@ elif ! [[ -t 0 ]]; then
 	PARAMS=""
 	while read ID; do
 		PARAMS+="&ids[imageids][]=$ID"
+		COUNT=$(( COUNT + 1 ))
 	done
 	PARAMS="ids[count]=$COUNT$PARAMS"
 else
 	exit 1
 fi
-docurl --data-raw "$PARAMS" 'https://app.roll20.net/image_library/permdelete/'
+if (($COUNT)); then
+	docurl --data-raw "$PARAMS" 'https://app.roll20.net/image_library/permdelete/'
+else
+	>&2 echo -e "\033[31mNo request because COUNT is 0\033[0"
+fi
