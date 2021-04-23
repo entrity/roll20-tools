8e53213537e25910540f656c88b10caf3aa9ef4a
Markham <quinesquines@gmail.com>
2021-04-24 06:27:13 +0800

    Pipe list to jq it output is a terminal

--- list.sh
+++ list.sh
@@ -1,12 +1,20 @@
 . shared.sh
 
-if (($#)); then
-	FOLDERID=$1
-	docurl "https://app.roll20.net/image_library/fetchlibraryfolder/$FOLDERID"
-elif [[ -n $TRUEORPHANS ]]; then
-	# Search for images which aren't in any folders
-	docurl 'https://app.roll20.net/image_library/fetchorphanassets/true/1'
+function main () {
+	if (($#)); then
+		FOLDERID=$1
+		docurl "https://app.roll20.net/image_library/fetchlibraryfolder/$FOLDERID"
+	elif [[ -n $TRUEORPHANS ]]; then
+		# Search for images which aren't in any folders
+		docurl 'https://app.roll20.net/image_library/fetchorphanassets/true/1'
+	else
+		# Search for all images
+		docurl 'https://app.roll20.net/image_library/fetchorphanassets/false/1'
+	fi
+}
+
+if [[ -t 1 ]]; then
+	main | jq
 else
-	# Search for all images
-	docurl 'https://app.roll20.net/image_library/fetchorphanassets/false/1'
+	main
 fi
