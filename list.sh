01817235bda4b61043881f6cb68a1accb9426b48
Markham <quinesquines@gmail.com>
2021-04-24 06:19:03 +0800

    List folder contents

--- list.sh
+++ list.sh
@@ -1,5 +1,12 @@
 . shared.sh
 
-docurl 'https://app.roll20.net/image_library/fetchorphanassets/true/1' 
-
-
+if (($#)); then
+	FOLDERID=$1
+	docurl "https://app.roll20.net/image_library/fetchlibraryfolder/$FOLDERID"
+elif [[ -n $TRUEORPHANS ]]; then
+	# Search for images which aren't in any folders
+	docurl 'https://app.roll20.net/image_library/fetchorphanassets/true/1'
+else
+	# Search for all images
+	docurl 'https://app.roll20.net/image_library/fetchorphanassets/false/1'
+fi
