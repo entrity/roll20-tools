f1c9382960c4358dd1e65006242bffc692197a01
Markham <quinesquines@gmail.com>
2021-04-24 15:09:13 +0800

    Correct output format for login

--- login.sh
+++ login.sh
@@ -6,9 +6,20 @@ login () {
 		>&2 echo "ERR USER or PASS undefined"
 		exit 1
 	fi
-	docurl "https://app.roll20.net/sessions/create" \
-	-d "email=${USER}&password=${PASS}" \
-  -v -q
+	>&2 echo -e "\033[33m${@}\033[0m"
+	local curlcmd=(
+		curl "https://app.roll20.net/sessions/create"
+		"${CURLARGS[@]}"
+		-d "email=${USER}&password=${PASS}"
+		-w "%{http_code}\t%{redirect_url}\n" -s -q
+	)
+	read STATUS REDIRECT < <("${curlcmd[@]}")
+	if [[ $REDIRECT =~ /editor/$ ]]; then
+		echo -e "\033[32m$STATUS\t$REDIRECT\033[0m"
+	else
+		echo -e "\033[31m$STATUS\t$REDIRECT\033[0m"
+		exit 1
+	fi
 }
 
 login
